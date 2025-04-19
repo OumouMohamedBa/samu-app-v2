59c7c2dfdb0e41d39c5cbb06f4bd0704af2e6d41
OumouMohamedBa <24408@esp.mr>
2025-04-19 20:46:36 +0200

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// src/app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Page non trouvée</h1>
+      <p>La page que vous cherchez n'existe pas.</p>
+    </div>
+  );
+}
